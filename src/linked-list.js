d01ea25b5563feba514c1b20f6acfc796c130aac
KatePavlovich <asylumhere@mail.ru>
2018-04-10 14:14:38 +0300

    start solving this task

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,15 +1,56 @@
 const Node = require('./node');
 
 class LinkedList {
-    constructor() {}
+    constructor() {
+		this.length = 0;
+		this._head = null;
+		this._tail = null;
+	}
 
-    append(data) {}
+    append(data) {
+		var node = new Node(data);
+ 
+    if (this.length) {
+        this._tail.next = node;
+        node.previous = this._tail;
+        this._tail = node;
+    } else {
+        this._head = node;
+        this._tail = node;
+    }
+ 
+    this.length++;
+ 
+    return node;
+	}
 
-    head() {}
+    head() {return this._head}
 
-    tail() {}
+    tail() {
+		var currentNode = this._head;
+		if (currentNode==null){return null;}
+		while (currentNode.next){
+			currentNode=currentNode.next;
+		}
+		return currentNode;}
 
-    at(index) {}
+    at(index) {
+		var currentNode = this._head,
+        length = this.length,
+        count = 1,
+        message = {failure: 'Failure: non-existent node in this list.'};
+ 
+    if (length === 0 || position < 1 || position > length) {
+        throw new Error(message.failure);
+    }
+
+    while (count < position) {
+        currentNode = currentNode.next;
+        count++;
+    }
+ 
+    return currentNode;
+	}
 
     insertAt(index, data) {}
 
