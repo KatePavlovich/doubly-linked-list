3f949723a0544214f5ac7e31e07a1fc733993086
KatePavlovich <asylumhere@mail.ru>
2018-04-15 09:57:45 +0300

    add insertAt and deleteAt methods

--- src/linked-list.js
+++ src/linked-list.js
@@ -55,23 +55,20 @@ class LinkedList {
 
 	
     insertAt(index, data) {
-		var node = new Node(data);
-	 
-		if (this.length===index) {
-			this._tail.next = node;
-			node.previous = this._tail;
-			this._tail = node;
-		} 
-		if (index===0){
-			this._head = node;
-			this._tail = node;
+		let node = new Node(data);
+		let count=0;
+		let currentNode=this._head;
+
+		while(count<index){
+			currentNode=currentNode.next;
+			count++;
 		}
-		else {
-			
-		}
-	 
+		currentNode.previous.next=node;
+		node.previous=currentNode.previous;
+		currentNode.previous=node;
+		node.next=currentNode;
+
 		this.length++;
-	 
 		return node;
 	}
 
@@ -86,79 +83,51 @@ class LinkedList {
 		this._tail.data = null;
 	}
 
-	/*
-	Let the node to be deleted is del.
-1) If node to be deleted is head node, then change the head pointer to next current head.
-2) Set next of previous to del, if previous to del exists.
-3) Set prev of next to del, if next to del exists.
-	*/
-	
 	
     deleteAt(index) {
 		let currentNode = this._head;
-		let length = this.length;
-		let count = 0;
-		const message = {failure: 'Failure: non-existent node in this list.'}
-		let beforeNodeToDelete = null
-		let nodeToDelete = null
-		let deletedNode = null
+		let count = 0;		
 	 
-		// 1st use-case: an invalid position
-		if (length === 0 || index < 0 || index > length) {
-		  throw new Error(message.failure)
+		while (count < index) {
+			currentNode = currentNode.next;
+			count++;
 		}
-		// 2nd use-case: the first node is removed
-		if (index === 1) {
-		  this._head = currentNode.next;
-		  // 2nd use-case: there is a second node
-		  if (!this._head) {
-			this._head.previous = null
-		  // 2nd use-case: there is no second node
-		  } else {
-			this._tail = null
-		  }
-		      // 3rd use-case: the last node is removed
-    } else if (index === this.length) {
-      this._tail = this._tail.previous
-      this._tail.next = null
-    // 4th use-case: a middle node is removed
-    } else {
-      while (count < index) {
-        currentNode = currentNode.next
-        count++
-      }
-      beforeNodeToDelete = currentNode.previous
-      nodeToDelete = currentNode
-      afterNodeToDelete = currentNode.next
-
-      beforeNodeToDelete.next = afterNodeToDelete
-      afterNodeToDelete.previous = beforeNodeToDelete
-      deletedNode = nodeToDelete
-      nodeToDelete = null
-    }
+	  	
+		currentNode.previous.next=currentNode.next;
+		currentNode.next.previous=currentNode.previous;
+		currentNode=null;
  
-    this.length--
-    
-    return message.success
-  }
+		this.length--;
+	}
+	
 
     reverse() {
-		let temp;
+		let temp=this._head;
+		
+		this._head=this._tail;
+		this._tail=temp;
+		this._head.next=this._head.previous;
 		let currentNode=this._head;
+		this._tail.next=null;
 		
 		while (currentNode!==null) {
 			//[currentNode.previous, currentNode.next]=[currentNode.next, currentNode.previous];
-			temp = currentNode.previous;
-            currentNode.previous = currentNode.next;
-            currentNode.next = temp;
-			currentNode=currentNode.previous;
-		}
-		 if (temp != null) {
-            this._head = temp.previous;
-        }
-		//this._head.previous=null;
-		//this._tail.next=null;
-		
+			
+
+			
+			temp = currentNode.next;
+            currentNode.next = currentNode.previous;
+            currentNode.previous = temp;
+			currentNode=currentNode.next;
+			
+			   //next  = current->next;  //temporarily store the next node
+			   //current->next = prev;    //now assign the prev!node to next of current node
+			   //prev = current;   //update the previous pointer
+			   //current = next;
+			
+			
+			
+		}		
 	}
 
 	
